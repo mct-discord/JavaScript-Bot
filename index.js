6470af73df5297be8e90ed21556511e74cd8067d
DemeulemeesterJarne <jarne.demeulemeester@student.howest.be>
2019-06-29 21:51:58 +0200

    On message optimizations

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content.startsWith(`${prefix}setup`)) {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+	if (command === 'setup') {
 		message.channel.send('TODO');
 	}
 });
