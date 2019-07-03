19f6a0492b90f5a39b079b005ab1ac35af3b0e11
DemeulemeesterJarne <jarne.demeulemeester@student.howest.be>
2019-07-03 23:05:03 +0200

    First call to canvas api

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const { Client } = require('discord.js');
-const { token, prefix } = require('./config.json');
+const { prefix, token, canvas_api_token } = require('./config.json');
+const fetch = require('node-fetch');
 const client = new Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
+client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 	if (command === 'setup') {
-		message.channel.send('TODO');
+		message.channel.send('WIP');
+		await fetch('https://leho-howest.instructure.com/api/v1/courses?access_token=' + canvas_api_token)
+			.then(response => response.json())
+			.then(json => message.channel.send(JSON.stringify(json[0])));
 	}
 });
 
