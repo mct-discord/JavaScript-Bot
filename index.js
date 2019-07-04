ad9fd3f86ea81c6f2c4091a37714afb0ea0a0eca
DemeulemeesterJarne <jarne.demeulemeester@student.howest.be>
2019-07-04 20:51:00 +0200

    Implement redis db + get courses en favorites

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { Client } = require('discord.js');
-const { prefix, token, canvas_api_token } = require('./config.json');
+const Keyv = require('keyv');
+const { prefix, token, redis_connection_string } = require('./config.json');
 const fetch = require('node-fetch');
+
 const client = new Client();
+const keyv = new Keyv(redis_connection_string);
+keyv.on('error', err => console.error('Keyv connection error:', err));
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -9,14 +13,32 @@ client.once('ready', () => {
 
 client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
-	if (command === 'setup') {
-		message.channel.send('WIP');
+	message.channel.send('WIP');
+	if (command === 'courses') {
+		const courses = [];
+		const canvas_api_token = await keyv.get('canvas_api_token');
 		await fetch('https://leho-howest.instructure.com/api/v1/courses?access_token=' + canvas_api_token)
 			.then(response => response.json())
-			.then(json => message.channel.send(JSON.stringify(json[0])));
+			.then(json => {
+				for (let i = 0; i < json.length; i++) {
+					courses.push(json[i].name);
+				}
+				message.channel.send(courses);
+			});
+	}
+	else if (command === 'favorites') {
+		const favorites = [];
+		const canvas_api_token = await keyv.get('canvas_api_token');
+		await fetch('https://leho-howest.instructure.com/api/v1/users/self/favorites/courses?access_token=' + canvas_api_token)
+			.then(response => response.json())
+			.then(json => {
+				for (let i = 0; i < json.length; i++) {
+					favorites.push(json[i].name);
+				}
+				message.channel.send(favorites);
+			});
 	}
 });
 
