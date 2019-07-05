f5a83eb22df25c0505b0f81465c33824f686e0ae
DemeulemeesterJarne <jarne.demeulemeester@student.howest.be>
2019-07-05 22:09:37 +0200

    Added basic profile command (needs a lot of cleanup)

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { Client } = require('discord.js');
+const Discord = require('discord.js');
 const Keyv = require('keyv');
 const { prefix, token, redis_connection_string } = require('./config.json');
 const fetch = require('node-fetch');
 
-const client = new Client();
+const client = new Discord.Client();
 const keyv = new Keyv(redis_connection_string);
 keyv.on('error', err => console.error('Keyv connection error:', err));
 
@@ -51,6 +51,31 @@ client.on('message', async message => {
 				}
 				const name = names.join(' ');
 				message.channel.send(name);
+			});
+	}
+	else if (command === 'profile') {
+		const courses = [];
+		const canvas_api_token = await keyv.get('canvas_api_token');
+		await fetch('https://leho-howest.instructure.com/api/v1/users/self?access_token=' + canvas_api_token)
+			.then(response => response.json())
+			.then(async json => {
+				const names = json.name.split(' ');
+				for (let i = 0; i < names.length; i++) {
+					names[i] = names[i].charAt(0).toUpperCase() + names[i].slice(1).toLowerCase();
+				}
+				const name = names.join(' ');
+				await fetch('https://leho-howest.instructure.com/api/v1/courses?access_token=' + canvas_api_token)
+					.then(response => response.json())
+					.then(jsonCourses => {
+						for (let i = 0; i < jsonCourses.length; i++) {
+							courses.push(jsonCourses[i].name);
+						}
+						const exampleEmbed = new Discord.MessageEmbed()
+							.setColor('#e85e00')
+							.setAuthor(name, json.avatar_url)
+							.addField('Courses', courses);
+						message.channel.send(exampleEmbed);
+					});
 			});
 	}
 });
