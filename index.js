36d5f5473e054c387ac2dace646cae390b9a7310
DemeulemeesterJarne <jarne.demeulemeester@student.howest.be>
2019-07-06 14:54:01 +0200

    Code cleanup

--- index.js
+++ index.js
@@ -11,72 +11,76 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+async function getCourses(canvas_api_token) {
+	const courses = [];
+	await fetch('https://leho-howest.instructure.com/api/v1/courses?access_token=' + canvas_api_token)
+		.then(response => response.json())
+		.then(json => {
+			for (let i = 0; i < json.length; i++) {
+				courses.push(json[i].name);
+			}
+		});
+	return courses;
+}
+
+async function getFavorites(canvas_api_token) {
+	const favorites = [];
+	await fetch('https://leho-howest.instructure.com/api/v1/users/self/favorites/courses?access_token=' + canvas_api_token)
+		.then(response => response.json())
+		.then(json => {
+			for (let i = 0; i < json.length; i++) {
+				favorites.push(json[i].name);
+			}
+		});
+	return favorites;
+}
+
+async function getName(canvas_api_token) {
+	let name = '';
+	await fetch('https://leho-howest.instructure.com/api/v1/users/self?access_token=' + canvas_api_token)
+		.then(response => response.json())
+		.then(json => {
+			const names = json.name.split(' ');
+			for (let i = 0; i < names.length; i++) {
+				names[i] = names[i].charAt(0).toUpperCase() + names[i].slice(1).toLowerCase();
+			}
+			name = names.join(' ');
+		});
+	return name;
+}
+
+async function getUser(canvas_api_token) {
+	let user = '';
+	await fetch('https://leho-howest.instructure.com/api/v1/users/self?access_token=' + canvas_api_token)
+		.then(response => user = response.json());
+	return user;
+}
+
 client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 	message.channel.send('WIP');
 	if (command === 'courses') {
-		const courses = [];
 		const canvas_api_token = await keyv.get('canvas_api_token');
-		await fetch('https://leho-howest.instructure.com/api/v1/courses?access_token=' + canvas_api_token)
-			.then(response => response.json())
-			.then(json => {
-				for (let i = 0; i < json.length; i++) {
-					courses.push(json[i].name);
-				}
-				message.channel.send(courses);
-			});
+		message.channel.send(await getCourses(canvas_api_token));
 	}
 	else if (command === 'favorites') {
-		const favorites = [];
 		const canvas_api_token = await keyv.get('canvas_api_token');
-		await fetch('https://leho-howest.instructure.com/api/v1/users/self/favorites/courses?access_token=' + canvas_api_token)
-			.then(response => response.json())
-			.then(json => {
-				for (let i = 0; i < json.length; i++) {
-					favorites.push(json[i].name);
-				}
-				message.channel.send(favorites);
-			});
+		message.channel.send(await getFavorites(canvas_api_token));
 	}
 	else if (command === 'name') {
 		const canvas_api_token = await keyv.get('canvas_api_token');
-		await fetch('https://leho-howest.instructure.com/api/v1/users/self?access_token=' + canvas_api_token)
-			.then(response => response.json())
-			.then(json => {
-				const names = json.name.split(' ');
-				for (let i = 0; i < names.length; i++) {
-					names[i] = names[i].charAt(0).toUpperCase() + names[i].slice(1).toLowerCase();
-				}
-				const name = names.join(' ');
-				message.channel.send(name);
-			});
+		message.channel.send(await getName(canvas_api_token));
 	}
 	else if (command === 'profile') {
-		const courses = [];
 		const canvas_api_token = await keyv.get('canvas_api_token');
-		await fetch('https://leho-howest.instructure.com/api/v1/users/self?access_token=' + canvas_api_token)
-			.then(response => response.json())
-			.then(async json => {
-				const names = json.name.split(' ');
-				for (let i = 0; i < names.length; i++) {
-					names[i] = names[i].charAt(0).toUpperCase() + names[i].slice(1).toLowerCase();
-				}
-				const name = names.join(' ');
-				await fetch('https://leho-howest.instructure.com/api/v1/courses?access_token=' + canvas_api_token)
-					.then(response => response.json())
-					.then(jsonCourses => {
-						for (let i = 0; i < jsonCourses.length; i++) {
-							courses.push(jsonCourses[i].name);
-						}
-						const exampleEmbed = new Discord.MessageEmbed()
-							.setColor('#e85e00')
-							.setAuthor(name, json.avatar_url)
-							.addField('Courses', courses);
-						message.channel.send(exampleEmbed);
-					});
-			});
+		const user = await getUser(canvas_api_token);
+		const profile = new Discord.MessageEmbed()
+			.setColor('#e85e00')
+			.setAuthor(await getName(canvas_api_token), user.avatar_url)
+			.addField('Courses', await getCourses(canvas_api_token));
+		message.channel.send(profile);
 	}
 });
 
