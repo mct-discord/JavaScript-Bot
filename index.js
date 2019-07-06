2a2c3444ccbb368f4c58eae3e9c3b164639af5ec
DemeulemeesterJarne <jarne.demeulemeester@student.howest.be>
2019-07-06 22:20:24 +0200

    Changed canvas_api_token to canvas_access_token

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const fetch = require('node-fetch');
 const parseLinkHeader = require('parse-link-header');
 
 const client = new Discord.Client();
-const canvas_api_tokens = new Keyv(redis_connection_string, { namespace: 'canvas_api_tokens' });
-canvas_api_tokens.on('error', err => console.error('Keyv connection error:', err));
+const canvas_access_tokens = new Keyv(redis_connection_string, { namespace: 'canvas_access_tokens' });
+canvas_access_tokens.on('error', err => console.error('Keyv connection error:', err));
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-async function getCourses(canvas_api_token) {
+async function getCourses(canvas_access_token) {
 	const courses = [];
 	let next_url = 'https://leho-howest.instructure.com/api/v1/courses';
 	while (next_url !== undefined) {
 		await fetch(next_url, {
 			headers: {
-				'Authorization': 'Bearer ' + canvas_api_token,
+				'Authorization': 'Bearer ' + canvas_access_token,
 			},
 		})
 			.then(response => {
@@ -40,11 +40,11 @@ async function getCourses(canvas_api_token) {
 	return courses;
 }
 
-async function getFavorites(canvas_api_token) {
+async function getFavorites(canvas_access_token) {
 	const favorites = [];
 	await fetch('https://leho-howest.instructure.com/api/v1/users/self/favorites/courses', {
 		headers: {
-			'Authorization': 'Bearer ' + canvas_api_token,
+			'Authorization': 'Bearer ' + canvas_access_token,
 		},
 	})
 		.then(response => response.json())
@@ -56,11 +56,11 @@ async function getFavorites(canvas_api_token) {
 	return favorites;
 }
 
-async function getName(canvas_api_token) {
+async function getName(canvas_access_token) {
 	let name = '';
 	await fetch('https://leho-howest.instructure.com/api/v1/users/self', {
 		headers: {
-			'Authorization': 'Bearer ' + canvas_api_token,
+			'Authorization': 'Bearer ' + canvas_access_token,
 		},
 	})
 		.then(response => response.json())
@@ -74,11 +74,11 @@ async function getName(canvas_api_token) {
 	return name;
 }
 
-async function getUser(canvas_api_token) {
+async function getUser(canvas_access_token) {
 	let user = '';
 	await fetch('https://leho-howest.instructure.com/api/v1/users/self', {
 		headers: {
-			'Authorization': 'Bearer ' + canvas_api_token,
+			'Authorization': 'Bearer ' + canvas_access_token,
 		},
 	})
 		.then(response => user = response.json());
@@ -90,36 +90,36 @@ client.on('message', async message => {
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 	if (command === 'token') {
-		const reply = await message.channel.send('Checking Canvas api token...');
+		const reply = await message.channel.send('Checking Canvas Access Token...');
 		if (!(await getUser(args[0])).name) {
-			reply.edit(':x: Invalid Canvas api token');
+			reply.edit(':x: Invalid Canvas Access Token');
 			return;
 		}
-		await canvas_api_tokens.set(message.author.id, args[0]);
-		reply.edit(':white_check_mark: Canvas api token saved');
+		await canvas_access_tokens.set(message.author.id, args[0]);
+		reply.edit(':white_check_mark: Saved Canvas Access Token');
 	}
 	else {
-		const canvas_api_token = await canvas_api_tokens.get(message.author.id);
-		if (canvas_api_token === undefined) {
-			message.channel.send('First tell me your canvas api token using: !token <canvas_api_token>');
+		const canvas_access_token = await canvas_access_tokens.get(message.author.id);
+		if (canvas_access_token === undefined) {
+			message.channel.send('First tell me your Canvas Access Token using: ```!token <canvas_access_token>```');
 			return;
 		}
 		if (command === 'courses') {
-			message.channel.send(await getCourses(canvas_api_token));
+			message.channel.send(await getCourses(canvas_access_token));
 		}
 		else if (command === 'favorites') {
-			message.channel.send(await getFavorites(canvas_api_token));
+			message.channel.send(await getFavorites(canvas_access_token));
 		}
 		else if (command === 'name') {
-			message.channel.send(await getName(canvas_api_token));
+			message.channel.send(await getName(canvas_access_token));
 		}
 		else if (command === 'profile') {
 			const reply = await message.channel.send('Loading profile...');
-			const user = await getUser(canvas_api_token);
+			const user = await getUser(canvas_access_token);
 			const profile = new Discord.MessageEmbed()
 				.setColor('#e85e00')
-				.setAuthor(await getName(canvas_api_token), user.avatar_url)
-				.addField('Courses', await getCourses(canvas_api_token));
+				.setAuthor(await getName(canvas_access_token), user.avatar_url)
+				.addField('Courses', await getCourses(canvas_access_token));
 			reply.delete();
 			message.channel.send(profile);
 		}
