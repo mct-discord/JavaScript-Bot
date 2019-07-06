3aa25571eb320f812c6ac05b15e85329ca70af13
DemeulemeesterJarne <jarne.demeulemeester@student.howest.be>
2019-07-06 15:36:33 +0200

    Added token command

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const Keyv = require('keyv');
-const { prefix, token, redis_connection_string } = require('./config.json');
+const { prefix, discord_api_token, redis_connection_string } = require('./config.json');
 const fetch = require('node-fetch');
 
 const client = new Discord.Client();
-const keyv = new Keyv(redis_connection_string);
-keyv.on('error', err => console.error('Keyv connection error:', err));
+const canvas_api_tokens = new Keyv(redis_connection_string, { namespace: 'canvas_api_tokens' });
+canvas_api_tokens.on('error', err => console.error('Keyv connection error:', err));
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -61,27 +61,34 @@ client.on('message', async message => {
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 	message.channel.send('WIP');
-	if (command === 'courses') {
-		const canvas_api_token = await keyv.get('canvas_api_token');
-		message.channel.send(await getCourses(canvas_api_token));
+	if (command === 'token') {
+		await canvas_api_tokens.set(message.author.id, args[0]);
+		message.channel.send('Canvas api token saved');
 	}
-	else if (command === 'favorites') {
-		const canvas_api_token = await keyv.get('canvas_api_token');
-		message.channel.send(await getFavorites(canvas_api_token));
-	}
-	else if (command === 'name') {
-		const canvas_api_token = await keyv.get('canvas_api_token');
-		message.channel.send(await getName(canvas_api_token));
-	}
-	else if (command === 'profile') {
-		const canvas_api_token = await keyv.get('canvas_api_token');
-		const user = await getUser(canvas_api_token);
-		const profile = new Discord.MessageEmbed()
-			.setColor('#e85e00')
-			.setAuthor(await getName(canvas_api_token), user.avatar_url)
-			.addField('Courses', await getCourses(canvas_api_token));
-		message.channel.send(profile);
+	else {
+		const canvas_api_token = await canvas_api_tokens.get(message.author.id);
+		if (canvas_api_token === undefined) {
+			message.channel.send('First tell me your canvas api token using: !token <canvas_api_token>');
+			return;
+		}
+		if (command === 'courses') {
+			message.channel.send(await getCourses(canvas_api_token));
+		}
+		else if (command === 'favorites') {
+			message.channel.send(await getFavorites(canvas_api_token));
+		}
+		else if (command === 'name') {
+			message.channel.send(await getName(canvas_api_token));
+		}
+		else if (command === 'profile') {
+			const user = await getUser(canvas_api_token);
+			const profile = new Discord.MessageEmbed()
+				.setColor('#e85e00')
+				.setAuthor(await getName(canvas_api_token), user.avatar_url)
+				.addField('Courses', await getCourses(canvas_api_token));
+			message.channel.send(profile);
+		}
 	}
 });
 
-client.login(token);
+client.login(discord_api_token);
